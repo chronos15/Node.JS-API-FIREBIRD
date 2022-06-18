711dd831e0d4923380cc2c28705bc0c606d1910f
Mauricio Costa <94569213+chronos15@users.noreply.github.com>
2022-06-18 09:59:43 -0300

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,9 +1,9 @@
 const conn = require("./connection").getConnection();
 
-const getClientes = async (res) => {
+const getClientes = async (res, req) => {
   await conn.get(async (err, db) => {
     if (err) res.status(500).json({ ok: false, error: "Não foi possível conectar ao banco de dados" });
-    return db.query("SELECT ID_CODIGO, NOME FROM CLIENTES", (err, result) => {
+    return db.query("SELECT * FROM CLIENTES ORDER BY ID_CODIGO ASC", (err, result) => {
       if (err) res.status(500).json({ ok: false, error: "Não foi possível ler a Query" });
       if (typeof result === "object") {
         Object.entries(result).forEach((value, index) => {
@@ -19,12 +19,12 @@ const getClientes = async (res) => {
   });
 };
 
-module.exports = { getClientes };
+
 
 const getOrdem = async (res) => {
   await conn.get(async (err, db) => {
     if (err) res.status(500).json({ ok: false, error: "Não foi possível conectar ao banco de dados" });
-    return db.query("SELECT ID_NUMERO, NOMECLIENTE FROM ORDEMSERVICO", (err, result) => {
+    return db.query("SELECT ID_NUMERO, ID_CLIENTE, NOMECLIENTE, SITUACAO FROM ORDEMSERVICO", (err, result) => {
       if (err) res.status(500).json({ ok: false, error: "Não foi possível ler a Query" });
       if (typeof result === "object") {
         Object.entries(result).forEach((value, index) => {
@@ -40,6 +40,26 @@ const getOrdem = async (res) => {
   });
 };
 
-module.exports = { getOrdem };
+
+const getProdutos = async (res) => {
+  await conn.get(async (err, db) => {
+    if (err) res.status(500).json({ ok: false, error: "Não foi possível conectar ao banco de dados" });
+    return db.query("SELECT * FROM PRODUTOS", (err, result) => {
+      if (err) res.status(500).json({ ok: false, error: "Não foi possível ler a Query" });
+      if (typeof result === "object") {
+        Object.entries(result).forEach((value, index) => {
+          Object.entries(result[index]).forEach((val) => {
+            const buff = result[index][val[0]];
+            result[index][val[0]] = Buffer.isBuffer(buff) ? Buffer.from(buff).toString() : buff;
+          });
+        });
+      }
+      db.detach();
+      res.status(200).json(result);
+    });
+  });
+};
+
+module.exports = { getClientes, getOrdem, getProdutos };
 
 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const conn = require("./connection").getConnection();
 
 app.use(express.json({ limit: 5000 }));
 app.use(cors());
@@ -13,12 +14,33 @@ app.get(["/", "/api"], (req, res) => {
   res.status(200).json({ ok: true, message: "Node API Firebird" });
 });
 
-app.get("/api/clientes", async (req, res) => {
-  await controller.getClientes(res);
+app.get('/api/clientes/:id?', async (req, res) => {
+  let filter = '';
+  await conn.get(async (err, db) => {
+    if (err) res.status(500).json({ ok: false, error: "Não foi possível conectar ao banco de dados" });
+    if(req.params.id) filter = ' WHERE ID_CODIGO=' + parseInt(req.params.id);
+    return db.query('SELECT ID_CODIGO, NOME FROM CLIENTES ORDER BY ID_CODIGO ASC' + filter, res, (err, result) => {
+      if (err) res.status(500).json({ ok: false, error: "Não foi possível ler a Query" });
+      if (typeof result === "object") {
+        Object.entries(result).forEach((value, index) => {
+          Object.entries(result[index]).forEach((val) => {
+            const buff = result[index][val[0]];
+            result[index][val[0]] = Buffer.isBuffer(buff) ? Buffer.from(buff).toString() : buff;
+          });
+        });
+      }
+      db.detach();
+      res.status(200).json(result);
+    });
+  });
 });
 
 app.get("/api/ordem", async (req, res) => {
   await controller.getOrdem(res);
+});
+
+app.get("/api/produtos", async (req, res) => {
+  await controller.getProdutos(res);
 });
 
 const port = process.env.PORT || 3333;
